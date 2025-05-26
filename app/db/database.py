03cea46e745b732fac8202e85072a2b32cc6114c
shohrux508 <yigitalievshohrux@gmail.com>
2025-05-26 23:28:26 +0500

    database created

---
+++ app/db/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+
+from app.config import DATABASE_URL
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False
+)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_db():
+    async with async_session() as db:
+        yield db
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
