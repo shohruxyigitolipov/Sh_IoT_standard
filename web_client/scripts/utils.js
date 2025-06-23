407f2d8d42bf91f6f95c760fc898bec1def0d459
Shokhrukh Tolipov <55028895+Shorux@users.noreply.github.com>
2025-06-23 23:45:11 +0500

    Replace device status text with color indicator (#22)

--- web_client/scripts/utils.js
+++ web_client/scripts/utils.js
@@ -2,12 +2,16 @@ import {wsSender} from "./websocket.js";
 
 export function changeDeviceStatus(status, render) {
     const deviceStatus = document.getElementById("device_status");
+    if (!deviceStatus) return;
+
     if (status) {
-        deviceStatus.innerText = "Устройство активно";
+        deviceStatus.classList.remove("offline");
+        deviceStatus.classList.add("online");
         if (render) {
             wsSender.get_report();
         }
     } else {
-        deviceStatus.innerText = "Устройство не активно";
+        deviceStatus.classList.remove("online");
+        deviceStatus.classList.add("offline");
     }
 }
